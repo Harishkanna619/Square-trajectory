728f0067c3b4bbca1b3def89cfdd6dd5ed0159b0
Harishkanna619 <166996417+Harishkanna619@users.noreply.github.com>
2024-04-14 22:08:32 +0530

    Triangle trajectory.py

--- Triangle trajectory.py
+++ Triangle trajectory.py
@@ -14,15 +14,15 @@ print 'taking down 5 m'
 print ' move in a triangular trajectory of side length 10m at a height of 5m'
 
 print ' 1. moving from Point A to B of triangle ABC'
-# h^2=p^2+b^2, here square root of sum of square of p=8m and b=6m
+# h^2=p^2+b^2, here square root of sum of square of c=10
 # gives us the desired side length of 10m. 
-drone.position_set(8.0, 6.0, 5, relative=True)
+drone.position_set(10, 0, 0, relative=True)
 
 print ' 2. moving from Point B to C of triangle ABC'
-drone.position_set(-8.0, 6.0, 0, relative=True)
+drone.position_set(0, 10, 0, relative=True)
 
 print ' 3. moving back to home position  from Point C to A of triangle ABC'
-drone.position_set(0, -10, 0, relative=True)
+drone.position_set(-10, -10, 0, relative=True)
 
 print ' 4. Landing'
 drone.land(async=False)
